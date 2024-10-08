469d59461bb31409976e339088c7e6ad16aecb15
declanecr <151974319+declanecr@users.noreply.github.com>
2024-10-08 15:45:49 -0400

    Add files via upload

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,19 @@
-module.exports = {
+/**
+ * Database Configuration
+ * 
+ * This module exports an object containing the configuration settings
+ * for connecting to the ClearDB MySQL database on Heroku.
+ * 
+ * @module dbConfig
+ * @type {Object}
+ * @property {string} HOST - The hostname of the database server
+ * @property {string} USER - The username for database authentication
+ * @property {string} PASSWORD - The password for database authentication
+ * @property {string} DB - The name of the database to connect to
+ */
+export default {
     HOST: "us-cluster-east-01.k8s.cleardb.net",
     USER: "b4c42cb403eb0a",
     PASSWORD: "d3934b54",
-    DB: "heroku_28b8dc682c0181a"
-  };
+    DB: "heroku_28b8dc682c0181a",
+};
